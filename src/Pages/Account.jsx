29625db03dcfd92dc66e2f7761ae31b61c5d6e1b
emilyb93 <emilybennett93@hotmail.com>
2021-10-14 15:54:42 +0100

    added create user functionality

--- src/Pages/Account.jsx
+++ src/Pages/Account.jsx
@@ -6,6 +6,9 @@ const Account = () => {
   const { user, setUser } = useContext(UserContext);
   const [userList, setUserList] = useState([]);
   const [selectedUser, setSelectedUser] = useState(user);
+  const [inputUsername, setInputUsername] = useState("");
+  const [inputURL, setInputURL] = useState("");
+  const [newUser, setNewUser] = useState({});
 
   useEffect(() => {
     axios
@@ -22,10 +25,26 @@ const Account = () => {
         console.log(err);
       });
   }, []);
+
+  useEffect(() => {
+    console.log(newUser, "newUser");
+    axios({
+      method: "post",
+      url: "https://nc-marketplace.herokuapp.com/api/users",
+      data: newUser,
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [newUser]);
   return (
     <section>
       <p>Username: {user}</p>
       <form
+        id="log-in-form"
         action=""
         onSubmit={(e) => {
           e.preventDefault();
@@ -40,10 +59,48 @@ const Account = () => {
           }}
         >
           {userList.map((user) => {
-            return <option value={user.username}>{user.username}</option>;
+            return (
+              <option key={user.username + Math.random()} value={user.username}>
+                {user.username}
+              </option>
+            );
           })}
         </select>
         <button type="submit">Select User!</button>
+      </form>
+
+      <h2>Sign Up!</h2>
+      <form
+        id="sign-up-form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          setNewUser(() => {
+            return {
+              username: inputUsername,
+              avatar_url: inputURL,
+            };
+          });
+        }}
+      >
+        Username:{" "}
+        <input
+          type="text"
+          required
+          placeholder="Enter Username"
+          onChange={(e) => {
+            setInputUsername(e.target.value);
+          }}
+        />
+        <br />
+        Avatar Url:{" "}
+        <input
+          type="url"
+          placeholder="Paste URL here"
+          onChange={(e) => {
+            setInputURL(e.target.value);
+          }}
+        />
+        <button>Create Account</button>
       </form>
     </section>
   );
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { useContext } from "react";
 import { UserContext } from "../Constants/UserContext";
+import { useHistory } from "react-router-dom";
 
 const Homepage = () => {
   const { user } = useContext(UserContext);
+  const history = useHistory();
 
   return (
     <section>
       <h2>Welcome!</h2>
       {!user && (
         <>
-          <button>Login</button>
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              history.push("/account");
+            }}
+          >
+            Login
+          </button>
           <button>Sign up</button>
         </>
       )}
